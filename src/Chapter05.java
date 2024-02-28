5e12a043caba9d6923ea8f8e4f531eb9d0c25f99
TaiheiYamabe <91378148+Tyamabe@users.noreply.github.com>
2024-02-29 06:28:38 +0900

    Lesson2 Chapter5の確認問題

---
+++ src/Chapter05.java
@@ -0,0 +1,14 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	String str1 = "Taihei";
+	System.out.println(str1);
+	str1 = "Riko";
+	System.out.println(str1);
+	final int finalNum1 = 7;
+	System.out.println(finalNum1);
+	}
+
+}
